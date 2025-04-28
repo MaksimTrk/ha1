5112edd1e19f47e2b7e093493346d639cc92e1aa
MaksimTrk <maks.trko@gmail.com>
2025-04-28 16:54:32 +0300

    commit7

---
+++ script.js
@@ -0,0 +1,95 @@
+function interview() {
+    const wantsToWork = confirm("Чи хотіли б ви працювати у нас?");
+    
+    if (wantsToWork) {
+        let age = prompt("Введіть ваш вік:");
+        
+        if (age >= 18) {
+            let answer = prompt("Розв'яжіть задачу: 2 + 2(4 + 2) = ?");
+
+            if (answer == 2 + 2 * (4 + 2)) {
+                compareStrings()
+                alert("Вітаємо! Ви успішно пройшли перевірку.");
+
+                const phone = prompt("Будь ласка, введіть ваш номер телефону:");
+                
+                alert("Дякуємо за участь у співбесіді. Ми зв'яжемося з вами!");
+            } else {
+                alert("Перевірку не пройдено. Спробуйте ще!.");
+            }
+        } else {
+            alert("Вибачте, ви занадто молоді для цієї роботи.");
+        }
+    } else {
+        alert("Співбесіду завершено.");
+    }
+}
+
+function showDeveloperInfo() {
+    let lastName = "Трохименко";
+    let firstName = "Максим";
+    let position = "Розробник"; 
+
+    alert("Розробник: " + firstName + " " + lastName + "\nПосада: " + position);
+}
+
+function compareStrings() {
+    let str1 = prompt("Лаконічність це добре, але не в розмові з клієнтом, можете потренуватись тут. Введіть перший варіант:");
+    let str2 = prompt("Введіть другий варіант:");
+
+    switch (true) {
+        case (str1.length > str2.length):
+            alert("Перший рядок довший: " + str1);
+            break;
+        case (str1.length < str2.length):
+            alert("Другий рядок довший: " + str2);
+            break;
+        case (str1.length === str2.length):
+            alert("Рядки однакової довжини");
+            break;
+    }
+}
+
+function changeBackground() {
+    document.body.style.backgroundColor = "#f0f8ff"; 
+    setTimeout(() => {
+        document.body.style.backgroundColor = ""; 
+    }, 30000); 
+}
+
+function redirectPage() {
+    location.href = "https://www.example.com"; 
+}
+
+function changeContent() {
+    const element = document.getElementById("catalog-header");
+    document.write("<p>Це новий текст, доданий через document.write.</p>");
+}
+
+function insertNewElement() {
+    const newElement = document.createElement("p");
+    const textNode = document.createTextNode("Це новий абзац, доданий через JavaScript.");
+    newElement.appendChild(textNode);
+    document.body.appendChild(newElement); 
+    const newPrependElement = document.createElement("p");
+    const textPrependNode = document.createTextNode("Це новий абзац, доданий на початок документа.");
+    newPrependElement.appendChild(textPrependNode);
+    document.body.prepend(newPrependElement); 
+}
+
+function manipulateNodes() {
+    const newParagraph = document.createElement("x");
+    const textNode = document.createTextNode("Цей абзац доданий після існуючого елемента.");
+    newParagraph.appendChild(textNode);
+    const existingParagraph = document.querySelector("p"); 
+    existingParagraph.after(newParagraph); 
+    const replacementParagraph = document.createElement("p");
+    replacementParagraph.textContent = "Цей абзац замінить попередній.";
+    existingParagraph.replaceWith(replacementParagraph); 
+}
+
+function removeElement() {
+    const element = document.querySelector("x");
+    element.remove(); 
+}
+
